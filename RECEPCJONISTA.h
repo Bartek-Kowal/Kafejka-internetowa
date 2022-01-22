f0244c92f54b7f78731dba85e369cf3e758b6464
Bojownikjpjpjpjp <rzygamteczajp@gmail.com>
2022-01-22 19:06:49 +0100

    druga poprawa, bo do złej metody pisałem

--- RECEPCJONISTA.h
+++ RECEPCJONISTA.h
@@ -16,15 +16,15 @@ class RECEPCJONISTA
 {
 	public: std::vector<REZERWACJA*> _unnamed_REZERWACJA_;
 
-	public: bool sprawdz_wniosek(REZERWACJA aRezerwacja);
-
-public: void zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob) {
-	cout << "\n wniosek do rozpatrzenia:\n" << aRodzaj << "\n" << aData << "\n" << aCzas << "\n" << aOdwolanie_rezerwacji << "\n" << aRaIlosc_osobodzaj << "\n";
-	cout << "czy akceptujesz rezerwacje klienta? wprowadz 1 jesli tak, jesli nie wprowadz 0";
-	short numerek;
-	cin >> numerek;
-	return rumerek;
+	public: bool sprawdz_wniosek(REZERWACJA aRezerwacja) {
+		cout << "\n wniosek do rozpatrzenia:\n" << aRezerwacja.Rodzaj << "\n" << aRezerwacja.aData << "\n" << aRezerwacja.aCzas << "\n" << aRezerwacja.aOdwolanie_rezerwacji << "\n" << aRezerwacja.aRaIlosc_osobodzaj << "\n";
+		cout << "czy akceptujesz rezerwacje klienta? wprowadz 1 jesli tak, jesli nie wprowadz 0";
+		short numerek;
+		cin >> numerek;
+		return rumerek;
 	};
 };
 
+
+public: void zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob);
 #endif
