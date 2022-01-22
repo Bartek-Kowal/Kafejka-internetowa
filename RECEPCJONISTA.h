99c69121c850ae70be3ccaecf73aeab52d45a6bf
Bartek-Kowal <77107112+Bartek-Kowal@users.noreply.github.com>
2022-01-23 00:37:19 +0100

    Add files via upload

--- RECEPCJONISTA.h
+++ RECEPCJONISTA.h
@@ -17,14 +17,13 @@ class RECEPCJONISTA
 	public: std::vector<REZERWACJA*> _unnamed_REZERWACJA_;
 
 	public: bool sprawdz_wniosek(REZERWACJA aRezerwacja) {
-		cout << "\n wniosek do rozpatrzenia:\n" << aRezerwacja.Rodzaj << "\n" << aRezerwacja.aData << "\n" << aRezerwacja.aCzas << "\n" << aRezerwacja.aOdwolanie_rezerwacji << "\n" << aRezerwacja.aRaIlosc_osobodzaj << "\n";
-		cout << "czy akceptujesz rezerwacje klienta? wprowadz 1 jesli tak, jesli nie wprowadz 0";
-		short numerek;
-		cin >> numerek;
-		return rumerek;
-	};
+	cout << "\n wniosek do rozpatrzenia:\n" << aRezerwacja._rodzaj << "\n" << aRezerwacja._data << "\n" << aRezerwacja._czas << "\n" << aRezerwacja._odwolanie_rezerwacji << "\n" << aRezerwacja._ilosc_osob << "\n";
+	cout << "czy akceptujesz rezerwacje klienta? wprowadz 1 jesli tak, jesli nie wprowadz 0";
+	short numerek;
+	cin >> numerek;
+	return numerek;
+}
+public: void zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob);
 };
 
-
-public: void zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob);
 #endif
--- SALA.h
+++ SALA.h
@@ -13,19 +13,19 @@ class SALA;
 
 class SALA
 {
-	public: int _nr_sali;
-	public: bool _czy_wolna;
-	public: int _godziny_rezerwacji[10];
-	public: int _pojemnosc;
-	public: std::vector<REZERWACJA*> _unnamed_REZERWACJA_;
+public: int _nr_sali;
+public: bool _czy_wolna;
+public: int _godziny_rezerwacji[10];
+public: int _pojemnosc;
+public: std::vector<REZERWACJA*> _unnamed_REZERWACJA_;
 
 public: bool czy_dostepna(int aIlosc_osob, string aRodzaj, int aData, int aCzas) {
 	return 1;
-	};
+};
 
-	public: bool wolne_sale(int aNr_sali, bool aCzy_wolna);
+public: bool wolne_sale(int aNr_sali, bool aCzy_wolna);
 
-	public: bool zarezerwuj_sale(REZERWACJA aRezerwacja);
+public: bool zarezerwuj_sale(REZERWACJA aRezerwacja);
 };
 
 #endif
