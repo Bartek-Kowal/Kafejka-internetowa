250c0695e7f1f14d84ee4f13bac62563961cd2fa
Bojownikjpjpjpjp <rzygamteczajp@gmail.com>
2022-01-23 02:01:42 +0100

    standaryzacja recepcjonisty

--- RECEPCJONISTA.cpp
+++ RECEPCJONISTA.cpp
@@ -6,11 +6,11 @@ using namespace std;
 #include "RECEPCJONISTA.h"
 #include "REZERWACJA.h"
 
-bool RECEPCJONISTA::sprawdz_wniosek(REZERWACJA aRezerwacja) {
+bool RECEPCJONISTA::Sprawdz_wniosek(REZERWACJA aRezerwacja) {
 	throw "Not yet implemented";
 }
 
-void RECEPCJONISTA::zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob) {
+void RECEPCJONISTA::Zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob) {
 	throw "Not yet implemented";
 }
 
--- RECEPCJONISTA.h
+++ RECEPCJONISTA.h
@@ -16,14 +16,14 @@ class RECEPCJONISTA
 {
 	public: std::vector<REZERWACJA*> _unnamed_REZERWACJA_;
 
-	public: bool sprawdz_wniosek(REZERWACJA aRezerwacja) {
+	public: bool Sprawdz_wniosek(REZERWACJA aRezerwacja) {
 	cout << "\n wniosek do rozpatrzenia:\n" << aRezerwacja._rodzaj << "\n" << aRezerwacja._data << "\n" << aRezerwacja._czas << "\n" << aRezerwacja._odwolanie_rezerwacji << "\n" << aRezerwacja._ilosc_osob << "\n";
 	cout << "czy akceptujesz rezerwacje klienta? wprowadz 1 jesli tak, jesli nie wprowadz 0";
 	short numerek;
 	cin >> numerek;
 	return numerek;
 }
-public: void zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob);
+public: void Zloz_wniosek(string aRodzaj, int aData, int aCzas, bool aOdwolanie_rezerwacji, int aIlosc_osob);
 };
 
 #endif
